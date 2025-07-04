51951b063d031a950a505a4404dbc92c938893a0
john681611 <john681611@hotmail.com>
2025-07-04 13:26:44 +0100

    Add Spawn points for Nevada Normandy and The Channel

--- tables/spawnPoints.lua
+++ tables/spawnPoints.lua
@@ -22,7 +22,7 @@ local maps = {
         zStart = 185000,
         zEnd = 947000
     },
-    Falklands = {  -- Done 04/07/2024
+    Falklands = { -- Done 04/07/2024
         x = 342234,
         xEnd = -421635,
         zStart = -1403914,
@@ -58,18 +58,18 @@ local maps = {
         zStart = -61329,
         zEnd = 138532
     },
-    -- Nevada = {
-    --     x = 100000,
-    --     xEnd = -500000,
-    --     zStart = 100000,
-    --     zEnd = 800000
-    -- },
-    -- Normandy = {
-    --     x = 100000,
-    --     xEnd = -500000,
-    --     zStart = 100000,
-    --     zEnd = 800000
-    -- },
+    Nevada = { -- Done 04/07/2024
+        x = -91264,
+        xEnd = -582440,
+        zStart = -367294,
+        zEnd = 174013
+    },
+    Normandy = { -- Done 04/07/2024
+        x = 247743,
+        xEnd = -124043,
+        zStart = -195360,
+        zEnd = 253654
+    },
     PersianGulf = { -- Done 04/07/2024
         x = 728050,
         xEnd = -286974,
@@ -88,12 +88,12 @@ local maps = {
         zStart = -424000,
         zEnd = 420000
     },
-    -- TheChannel = {
-    --     x = 100000,
-    --     xEnd = -500000,
-    --     zStart = 100000,
-    --     zEnd = 800000
-    -- },
+    TheChannel = { -- Done 04/07/2024
+        x = 70873,
+        xEnd = -126974,
+        zStart = -114175,
+        zEnd = 127616
+    },
 }
 local map = maps[theatre]
 local z = maps[theatre].zStart -- start x
