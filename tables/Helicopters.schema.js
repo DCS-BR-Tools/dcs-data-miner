7fc1dfc87155dcdee8bf8bce54cd37c2f560b2c0
john681611 <john681611@hotmail.com>
2025-10-03 22:35:26 +0100

    Fix Bad Task transform

--- tables/Helicopters.schema.js
+++ tables/Helicopters.schema.js
@@ -6,6 +6,7 @@ module.exports = zLuaArray(
     AddPropAircraft: zLuaArray(),
     payloadPresets: zLuaArray(zLuaObject({tasks: zLuaArray(), pylons: zLuaArray() })),
     tasks: zLuaArray(),
+    Tasks: zLuaArray(),
     paintSchemes: zLuaObject(),
     panelRadio: zLuaArray(zLuaObject({channels: zLuaArray() }))
   }),
--- tables/Planes.schema.js
+++ tables/Planes.schema.js
@@ -7,6 +7,7 @@ module.exports = zLuaArray(
     SpecificCallnames: zLuaObject({USA: zLuaArray()}),
     payloadPresets: zLuaArray(zLuaObject({tasks: zLuaArray(), pylons: zLuaArray() })),
     tasks: zLuaArray(),
+    Tasks: zLuaArray(),
     paintSchemes: zLuaObject(),
     panelRadio: zLuaArray(zLuaObject({channels: zLuaArray() }))
   }),
